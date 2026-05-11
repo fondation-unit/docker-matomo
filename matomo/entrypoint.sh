924ea85a3f51d4d5a4a4f6c08a9be7f425b606e0
Hipjea <pierre.duverneix@gmail.com>
2026-05-11 17:41:46 +0200

    php path

--- matomo/entrypoint.sh
+++ matomo/entrypoint.sh
@@ -4,7 +4,7 @@ touch /var/log/matomo-archive.log
 chown www-data:www-data /var/log/matomo-archive.log
 
 echo "MAILTO=\"${ACME_EMAIL}\"" > /etc/cron.d/matomo-archive
-echo "*/2 * * * * www-data php /var/www/html/console core:archive --url=https://${DOMAIN_NAME} >> /var/log/matomo-archive.log 2>&1" >> /etc/cron.d/matomo-archive
+echo "*/2 * * * * www-data /usr/local/bin/php /var/www/html/console core:archive --url=https://${DOMAIN_NAME} >> /var/log/matomo-archive.log 2>&1" >> /etc/cron.d/matomo-archive
 chmod 644 /etc/cron.d/matomo-archive
 
 rsyslogd

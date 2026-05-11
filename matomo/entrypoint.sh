5f4f619de8d8cb38bc69d163dee40df6679e4f91
Hipjea <pierre.duverneix@gmail.com>
2026-05-11 17:31:20 +0200

    cron

--- matomo/entrypoint.sh
+++ matomo/entrypoint.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
+touch /var/log/matomo-archive.log
+chown www-data:www-data /var/log/matomo-archive.log
+
 echo "MAILTO=\"${ACME_EMAIL}\"" > /etc/cron.d/matomo-archive
-echo "5 * * * * www-data php /var/www/html/console core:archive --url=https://${DOMAIN_NAME} >> /var/log/matomo-archive.log 2>&1" >> /etc/cron.d/matomo-archive
+echo "*/2 * * * * www-data php /var/www/html/console core:archive --url=https://${DOMAIN_NAME} >> /var/log/matomo-archive.log 2>&1" >> /etc/cron.d/matomo-archive
 chmod 644 /etc/cron.d/matomo-archive
+service rsyslog start
 cron -f
